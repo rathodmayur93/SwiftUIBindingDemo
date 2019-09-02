5a043aaffb6d1ca4a5a9db32fd4add1e0e22d944
ds-mayur <mayur.rathod@devjisystems.com>
2019-09-03 00:02:17 +0300

    Initial Commit

---
+++ Binding-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Binding-Demo
+//
+//  Created by ds-mayur on 9/3/19.
+//  Copyright © 2019 Mayur Rathod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
